e25283486e39955259b812e76a788a7028c8e58a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-15 16:59:40 +0000

    automatically backed up by learn

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,26 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.new(name: params[:title][:name])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+end
